83dc94757dfbde14e55a075ae01fe60bb5e00914
Виктория <viktoriya.pimkova@mail.ru>
2020-12-22 16:49:57 +0300

    сайт по подбору питания - добавлен рабочий таймер

--- js/Script.js
+++ js/Script.js
@@ -1,6 +1,8 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+        //tabs
+
     const choiceOfFoodStyle = document.querySelectorAll('.tabheader__item');
     const foodStyleDescription = document.querySelectorAll('.tabcontent');
     const tabsParent = document.querySelector('.tabheader__items');
@@ -26,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
     tabsParent.addEventListener('click', (event) => {
         const eventTarget = event.target;
 
-        if (eventTarget && eventTarget.classList.contains('tabheader__item')) {
+        if (eventTarget.classList.contains('tabheader__item')) {
             choiceOfFoodStyle.forEach((item, i) => {
                 if (item === eventTarget) {
                     hideTabContent();
@@ -35,5 +37,56 @@ document.addEventListener('DOMContentLoaded', () => {
             })
         }
     })
+
+        //timer
+
+    const timer = document.querySelector('.timer');
+    const daysCounter = document.querySelector('#days');
+    const hoursCounter = document.querySelector('#hours');
+    const minutesCounter = document.querySelector('#minutes');
+    const secondsCounter = document.querySelector('#seconds');
+    const deadline = '2021-01-01';
+
+    function getTimeRemaining (endTime) {
+        const parseRestOfTime = Date.parse(endTime) - Date.parse(new Date());
+        const daysCounter = Math.floor(parseRestOfTime / (1000 * 60 * 60 * 24 ));
+        const hoursCounter = Math.floor((parseRestOfTime / (1000 * 60 * 60)) % 24);
+        const minutesCounter = Math.floor((parseRestOfTime / (1000 * 60)) % 60);
+        const secondsCounter = Math.floor((parseRestOfTime / 1000) % 60)
+
+        return {
+            total: parseRestOfTime,
+            daysCounter: daysCounter,
+            hoursCounter: hoursCounter,
+            minutesCounter: minutesCounter,
+            secondsCounter: secondsCounter
+        }
+    }
+
+    function setClock (endTime) {
+
+        const timer = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock () {
+            const restOfTime = getTimeRemaining(endTime);
+
+            daysCounter.innerHTML = '' + restOfTime.daysCounter;
+            hoursCounter.innerHTML = '' + restOfTime.hoursCounter;
+            minutesCounter.innerHTML = '' + restOfTime.minutesCounter;
+            secondsCounter.innerHTML = '' + restOfTime.secondsCounter;
+
+            if (restOfTime.total <= 0) {
+                clearInterval(timer);
+            }
+
+            // if () {
+            //
+            // }
+        }
+    }
+
+    setClock(deadline);
 
 })
