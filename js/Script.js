5261da823398a7a89678ef9370aa93a44be41189
Виктория <viktoriya.pimkova@mail.ru>
2020-12-23 16:36:14 +0300

    улучшеныы карточки меню (практика работы с классами)

--- js/Script.js
+++ js/Script.js
@@ -135,8 +135,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const modalTimeout = setTimeout(openModal, 7000);
 
     function showModalByScroll () {
-        console.log(modalTimeout);
-        debugger;
         if ((window.pageYOffset + document.documentElement.clientHeight) >= document.documentElement.scrollHeight) {
             openModal();
             removeEventListener('scroll', showModalByScroll);
@@ -149,27 +147,34 @@ document.addEventListener('DOMContentLoaded', () => {
     //Введению в работу с классами на примере создания на странице карточек меню
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
             this.parent = document.querySelector(parentSelector);
+            this.classes = classes
         }
 
         render() {
             const element = document.createElement('div');
+
+            if (this.classes.length === 0) {
+                this.classes = "menu__item";
+                element.classList.add("menu__item")
+            } else {
+                this.classes.forEach(className => element.classList.add(className))
+            }
+
             element.innerHTML = `
-                <div class="menu__item">
-                        <img src= ${this.src} alt=${this.alt}>
-                        <h3 class="menu__item-subtitle">${this.title}</h3>
-                        <div class="menu__item-descr">${this.descr}</div>
-                        <div class="menu__item-divider"></div>
-                        <div class="menu__item-price">
-                            <div class="menu__item-cost">Цена:</div>
-                            <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
-                        </div>
+                    <img src= ${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
                     </div>
             `;
             this.parent.append(element);
