145c31aeed6587cd0a33abecda7a52cc7fa03ca6
Виктория <viktoriya.pimkova@mail.ru>
2021-01-15 14:34:27 +0300

    добавлена форма для обращения пользователя c использованием fetch

--- js/Script.js
+++ js/Script.js
@@ -229,6 +229,8 @@ document.addEventListener('DOMContentLoaded', () => {
         postData(item);
     })
 
+
+
     function postData (form) {
         form.addEventListener('submit', (event) => {
             event.preventDefault();
@@ -241,11 +243,6 @@ document.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'application/json');
 
             const formData = new FormData(form);
 
@@ -254,20 +251,25 @@ document.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
+            fetch('server.php', {
+                method: "POST",
+                body: JSON.stringify(object),
+                headers: {
+                    'Content-type': 'application/json'
                 }
             })
+                .then(data => data.text())
+                .then(data => {
+                    console.log(data);
+                    showThanksModal(message.success);
+                    statusMessage.remove();
+                })
+                .catch(() => {
+                    showThanksModal(message.failure);
+                })
+                .finally(() => {
+                    form.reset();
+                });
         })
     }
     
@@ -294,6 +296,5 @@ document.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000)
     }
-
 
 });
